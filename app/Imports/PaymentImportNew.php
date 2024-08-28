cba7ca4937b2e1569b4bbf5adbba50782961230a
Nicola <galdiolo.nicola@gmail.com>
2024-08-28 18:24:14 +0200

    Avanzamento import iscrizioni

--- app/Imports/PaymentImportNew.php
+++ app/Imports/PaymentImportNew.php
@@ -37,7 +37,7 @@ class PaymentImportNew implements ToCollection, WithStartRow
 
         $athlete = null;
 
-        $rows = collect($collection->reduce(function($arr, $item) use(&$athlete){
+        $athleteRows = collect($collection->reduce(function($arr, $item) use(&$athlete){
             $date_item = (int) $item[1];
             $date_obj = $date_item ? Date::excelToDateTimeObject($date_item) : null;
             if($date_obj){
@@ -64,33 +64,66 @@ class PaymentImportNew implements ToCollection, WithStartRow
 
             return $arr;
             
-        }, []));
+        }, []))->groupBy('athlete_id');
 
-        $rows->each(function($item){
-            
-            if($item['causal'] != 'Pagato'){
-                $row_fee_amount = $item['amount'];
-                if($row_fee_amount < 0){
-                    dd("c'è qualcosa che non va");
-                }else{
-                    $race_name = (explode(' - ', trim($item['causal'])))[0];
-                    $fee = Race::where('name', 'like', $race_name)->firstOrFail()->fees()->firstOrFail();
-                    
-                    $athlete = $item['athlete'];
+        $athleteRows->each(function($rows, $athleteKey){
 
-                    $athlete->fees()->syncWithoutDetaching(
-                        [
-                            $fee->id => [
-                                'custom_amount' => $row_fee_amount,
-                                'created_at' => $item['date']
-                            ]
-                        ]
-                    );
-                }
+            $fees_to_pay = collect([]);
+            $resume = 0;
 
-            }else{
-                $i = "devi registrare il pagamento";
+            $athlete = Athlete::findOrFail($athleteKey);
+
+            if($athlete->id == 5){
+                $i = "test";
             }
+
+            $rows->each(function($item) use($athlete, $fees_to_pay, &$resume){
+                
+                if($item['causal'] != 'Pagato'){
+                    $row_fee_amount = $item['amount'];
+                    if($row_fee_amount < 0){
+                        dd("c'è qualcosa che non va");
+                    }else{
+                        $race_name = (explode(' - ', trim($item['causal'])))[0];
+                        $fee = Race::where('name', 'like', $race_name)->firstOrFail()->fees()->firstOrFail();
+                        
+                        $fees_to_pay->put($fee->id, $fee);
+
+                        $athlete->fees()->syncWithoutDetaching(
+                            [
+                                $fee->id => [
+                                    'custom_amount' => $row_fee_amount,
+                                    'created_at' => $item['date']
+                                ]
+                            ]
+                        );
+                    }
+    
+                }else{
+                    $resume = ($resume + abs($item['amount']));
+
+                    $fees_to_pay->each(function($fee, $feeKey) use($athlete, $item, $fees_to_pay, &$resume){
+                        $athletefee = $athlete->fees()->findOrFail($fee->id)->athletefee;
+                        if($resume >= $athletefee->custom_amount){
+                            //ho budget per pagare la gara, registro il pagamento e scalo il residuo
+                            $resume = ($resume - $athletefee->custom_amount);
+                            $athletefee->update([
+                                'payed_at' => $item['date']
+                            ]);
+                            $fees_to_pay->forget($feeKey);
+                        }//else{
+                        //    dd($athlete->fullname, "c'è qualcosa che non va, nn ho più soldi");
+                        //}
+                    });
+                }
+            });
+
+            //if($resume > 0 && $athlete->id != 48 && $athlete->id != 59){
+            if($resume > 0){
+                $i = $athlete;
+                dd($athlete->id, $athlete->fullname, "c'è qualcosa che non va, ho pagato di più");
+            }
+        
         });
     }
 }
