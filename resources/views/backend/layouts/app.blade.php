9a59aa6978d31f679bbf56522bf10f6fe25d5205
Chloe <Chloe@Chloe.local>
2024-11-18 18:44:55 +0100

    Ottimizzazione layout

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -3,16 +3,11 @@
 
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="icon" type="image/png" href="{{asset('img/logo.png')}}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('img/logo.png')}}">
-    <meta name="keyword" content="aaaa">
-    <meta name="description" content="aaaa">
-
-    <!-- Shortcut Icon -->
-    <link rel="shortcut icon" href="{{asset('img/logo.png')}}">
-    <link rel="icon" type="image/ico" href="{{asset('img/logo.png')}}" />
+    <!-- Aggiunta icona per dispositivi mobile -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('img/apple-touch-icon.png')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
