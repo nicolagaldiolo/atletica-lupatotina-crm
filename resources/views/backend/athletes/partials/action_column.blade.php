fea2eac5c584862ae1f2db9650aef36bcaebc5ae
Nicola Galdiolo <galdiolo.nicola@gmail.com>
2026-02-08 23:09:42 +0100

    Fix permessi abbigliamento

--- resources/views/backend/athletes/partials/action_column.blade.php
+++ resources/views/backend/athletes/partials/action_column.blade.php
@@ -70,12 +70,14 @@
                         </li>
                     @endcan
 
-                    <li class="nav-item">
-                        <a class="nav-link @if(Route::is('athletes.orders.*')) active @endif" aria-current="page" href="{{ route("athletes.orders.index", $athlete) }}">
-                            <i class="fas fa-shirt"></i>
-                            {{ __('Abbigliamento') }}
-                        </a>
-                    </li>
+                    @can('viewAny', [App\Models\Order::class, $athlete])
+                        <li class="nav-item">
+                            <a class="nav-link @if(Route::is('athletes.orders.*')) active @endif" aria-current="page" href="{{ route("athletes.orders.index", $athlete) }}">
+                                <i class="fas fa-shirt"></i>
+                                {{ __('Abbigliamento') }}
+                            </a>
+                        </li>
+                    @endcan
                 </ul>
             </div>
         </div>
@@ -100,9 +102,9 @@
         @can('viewAny', App\Models\Voucher::class)
             <x-backend.buttons.edit route='{{ route("athletes.vouchers.index", $athlete) }}' small="true" icon="fas fa-tags" title="{{ __('Voucher') }}"/>
         @endcan
-        {{--@can('viewAny', App\Models\Voucher::class)--}}
+        @can('viewAny', [App\Models\Order::class, $athlete])
             <x-backend.buttons.edit route='{{ route("athletes.orders.index", $athlete) }}' small="true" icon="fas fa-shirt" title="{{ __('Abbigliamento') }}"/>
-        {{--@endcan--}}
+        @endcan
         @can('delete', $athlete)
             <x-backend.buttons.delete route='{{ route("athletes.destroy", $athlete) }}' small="true" data_confirm='Sei sicuro?' data_method="DELETE" title="{{ __('Elimina') }}" data_token="{{csrf_token()}}"/>
         @endcan
